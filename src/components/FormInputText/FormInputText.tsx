a1bdc8edece4df85146c7e9d4af196c5378f9c7a
Jonwbdv <jonathanssanthosh@gmail.com>
2024-02-21 14:53:00 -0600

    completed initial component for non-dropdown text input

--- src/components/FormInputText/FormInputText.tsx
+++ src/components/FormInputText/FormInputText.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react"
+import { EventHandler, useState } from "react"
 import "./FormInputText.css"
 
 interface FormInputTextProps{
     labelText: string,
-    inputType?: string
+    inputType?: string,
+    customStyles?: string
 }
 
 interface StyleObjectLabelType {
@@ -11,26 +12,31 @@ interface StyleObjectLabelType {
     opacity: number
 }
 
-const FormInputText = ({labelText, inputType = "regular"}: FormInputTextProps) => {
+const FormInputText = ({labelText, inputType = "regular", customStyles}: FormInputTextProps) => {
     const [focused, setIsFocused] = useState<boolean>(false)
     const [styleObjectInput, setStyleObjectInput] = useState<StyleObjectLabelType>({opacity: 0.6})
     console.log(focused)
 
     const onFocusHandlerInput = () => {
         setIsFocused(true)
-        setStyleObjectInput({...styleObjectInput, transform: "translateY(-50%)", opacity: 1})
+        setStyleObjectInput({...styleObjectInput, transform: "translateY(-80%)", opacity: 1})
     }
-    const onBlurHandlerInput = () => {
+    const onBlurHandlerInput = (e: React.FocusEvent<HTMLInputElement>) => {
         setIsFocused(false)
-        setStyleObjectInput({...styleObjectInput, transform: "translateY(0%)", opacity: 0.6})
+        console.log(e.target.value)
+        if(e.target.value.length === 0){
+            setStyleObjectInput({...styleObjectInput, transform: "translateY(0%)", opacity: 0.6})
+        }
     }
     return (
-        <section className="form-input-text-wrapper w-full flex flex-col justify-center relative">
+        <section className={`form-input-text-wrapper flex flex-col justify-center relative ${customStyles}`}>
             <label className="absolute" htmlFor={`${labelText}`} style={styleObjectInput} > {labelText}</label>
-            {inputType === "regular" && <input type="text" className="form-input-text" onFocus={onFocusHandlerInput} onBlur={onBlurHandlerInput}/>}
-            {inputType === "email" && <input type="email" className="form-input-text" onFocus={onFocusHandlerInput} onBlur={onBlurHandlerInput}/>}
-            {inputType === "password" && <input type="password" className="form-input-text" onFocus={onFocusHandlerInput} onBlur={onBlurHandlerInput}/>}
-            <div className={`input-underline-bar ${focused ? "opacity-[1]" : "opacity-[0.6]"}`}/>
+            {inputType === "text" && <input type={inputType} className="form-input-text" onFocus={onFocusHandlerInput} onBlur={onBlurHandlerInput}/>}
+            {inputType === "email" && <input type={inputType} className="form-input-text" onFocus={onFocusHandlerInput} onBlur={onBlurHandlerInput}/>}
+            {inputType === "password" && <input type={inputType} className="form-input-text" onFocus={onFocusHandlerInput} onBlur={onBlurHandlerInput}/>}
+            <div className="input-underline-bar-background">
+                <div className={`input-underline-bar-foreground ${focused ? "w-full h-full": "h-0 w-0"}`}/>    
+            </div>
         
         </section>
     )
--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { Form } from 'react-router-dom'
+import { Form, Link } from 'react-router-dom'
 import EuphoriaLogo from '../../components/EuphoriaLogo/EuphoriaLogo'
 import FormInputText from '../../components/FormInputText/FormInputText'
 import './Login.css'
@@ -13,8 +13,9 @@ const Login = () => {
                     
                     <form id="login-form-wrapper" className='w-[75%]'>
                         <h1 id="main-title">Login</h1>
-                        <FormInputText labelText='Email'/>
-                        <FormInputText labelText='Password' />
+                        <FormInputText labelText='Email' inputType='email' customStyles='w-[50%]'/>
+                        <FormInputText labelText='Password' inputType='password' customStyles='w-[50%]'/>
+                        <Link to="/reset-password-request">Reset password</Link>
                     </form>
 
 
